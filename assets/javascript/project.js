2403c97ecd348dce63bb821e5559f5592fe1c3dc
JBW <whitco670@jbw-mbp-215.attlocal.net>
2017-12-02 06:28:58 -0500

    pic and rating are loading but empty.clear function is not working

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,33 +1,65 @@
 console.log("hello world of giphy");
 
 var topics = ["surfing", "skateboarding", "hang gliding", "snowboarding","parachuting","jet ski"];
-//create a loop to create buttons
+//build a loop to create buttons
 
 
 function renderButtons(){
 
 	$("#topics-view").empty();
+
 	for (var i = 0;i < topics.length; i++)
 	{
 
-	var a = $("<button>");
+		var a = $("<button>");
 	
-	 a.addClass("sport");
-	// $(this).attr("data-name")
-	 a.attr("data-name", topics[i]);
+	 	a.addClass("sport");
+	
+	 	a.attr("data-name", topics[i]);
 
-	 a.text(topics[i]);
+	 	a.text(topics[i]);
 
-
-	 $("#topics-view").append(a);
+	 	$("#topics-view").append(a);
 	 
-	 
+	} 
 	
-	
-	}
 }
+	renderButtons()
+
+
+	$("button").on("click", function(){
+		
+		var sport = $(this).attr("data-name");
+		console.log(sport);
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ sport +"&rating=pg&api_key=dc6zaTOxFJmzC&limit=10";
+
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    })
+    .done(function(response) {
+      console.log(response.data);
+      
+      for (var i = 0; i < 10; i++){
+      	var sportDiv = $("<div>");
+      	var p = $("<p>").text("Rating: "+ response.data[i].rating);
+      	console.log(p);
+      	var sportImage = $("<img>");
+      	
+      	sportImage.attr("src",response.data[i].images.fixed_height_still.url);
+      	sportDiv.append(p);
+      	
+      	sportDiv.append(sportImage);
+      	$("#gifs-appear-here").prepend(sportDiv);
+      
+        console.log(response.data[i].images.fixed_height_still);
+        }
+      });
+
+	});	
+	//renderButtons();
 
 	//event.preventDefault();
 
-renderButtons();
+//renderButtons();
 
